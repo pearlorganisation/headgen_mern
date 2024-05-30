f5252d162f6cc799206096a8f6f89813fd2f04c8
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-05-30 18:09:33 +0530

    worked teams section

--- Frontend/src/pages/Blog/BlogDetails.jsx
+++ Frontend/src/pages/Blog/BlogDetails.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 const BlogDetails = () => {
     return (
         <div className='container mx-auto min-h-screen py-28'>
-            <div className=' max-w-6xl  mx-auto text-white space-y-4'>
-                <img className='rounded-md' src="https://www.themultiverse.ai/_next/image?url=https%3A%2F%2Fthe-multiverse-ai.ghost.io%2Fcontent%2Fimages%2F2023%2F11%2Fno-watermark---team-gallery--1-.webp&w=1920&q=75" alt="" />
+            <div className=' max-w-4xl  mx-auto text-white space-y-4'>
+                <img className='rounded-md ' src="https://www.themultiverse.ai/_next/image?url=https%3A%2F%2Fthe-multiverse-ai.ghost.io%2Fcontent%2Fimages%2F2023%2F11%2Fno-watermark---team-gallery--1-.webp&w=1920&q=75" alt="" />
                 <h1 className='text-3xl max-w-2xl '>21 Best “Meet the Team” Examples and Why They Work
                     Updated on May 9, 2024</h1>
+                <p>The "Meet the Team" gallery is a dynamic display showcasing the team's diversity and skills. This gallery goes beyond a simple staff introduction page; it presents an in-depth look at the team member bios, offering stories of dedication and expertise that resonate with visitors. The "Employee Spotlight Gallery" and "Team Bio Showcase" sections are particularly engaging, providing a rich narrative of each team member.
+
+                    Sections like "The Team Members" and "Employee Professional Profiles" are designed to connect personally with visitors. The "Team Member Highlights" segment offers a deeper understanding of each individual's contributions, illustrating how their unique skills and experiences enhance the team's overall dynamics.</p>
+                <p>The "Meet the Team" gallery is a dynamic display showcasing the team's diversity and skills. This gallery goes beyond a simple staff introduction page; it presents an in-depth look at the team member bios, offering stories of dedication and expertise that resonate with visitors. The "Employee Spotlight Gallery" and "Team Bio Showcase" sections are particularly engaging, providing a rich narrative of each team member.
+
+                    Sections like "The Team Members" and "Employee Professional Profiles" are designed to connect personally with visitors. The "Team Member Highlights" segment offers a deeper understanding of each individual's contributions, illustrating how their unique skills and experiences enhance the team's overall dynamics.</p>
                 <p>The "Meet the Team" gallery is a dynamic display showcasing the team's diversity and skills. This gallery goes beyond a simple staff introduction page; it presents an in-depth look at the team member bios, offering stories of dedication and expertise that resonate with visitors. The "Employee Spotlight Gallery" and "Team Bio Showcase" sections are particularly engaging, providing a rich narrative of each team member.
 
                     Sections like "The Team Members" and "Employee Professional Profiles" are designed to connect personally with visitors. The "Team Member Highlights" segment offers a deeper understanding of each individual's contributions, illustrating how their unique skills and experiences enhance the team's overall dynamics.</p>
