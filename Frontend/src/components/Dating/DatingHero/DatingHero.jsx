d546929f897c0add58fe1526dd19092b47f8bfe1
jai <jai@pearlorganisation.com>
2024-06-08 16:20:39 +0530

    updatse

--- Frontend/src/components/Dating/DatingHero/DatingHero.jsx
+++ Frontend/src/components/Dating/DatingHero/DatingHero.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const DatingHero = () => {
   return (
-    <div className='flex justify-between'>
-      <div className='flex flex-col justify-center gap-6 w-1/2'>
-            <div className='text-[55px] 2xl:text-[72px] text-[#F1F1F1]'>
-            <span className='font-semibold bg-gradient-to-r from-[#e73e71] to-[#af1040]  inline-block text-transparent bg-clip-text'>Double</span> your matches with our AI dating photos
-            </div>
-            <div className='text-[14px] 2xl:text-[16px] text-[#F1F1F1]'>
-            Use our AI headshot generator built by AI researchers to improve your dating profile in just minutes. Look your best to attract the best.
-            </div>
-            <div>
-            <Link to="/upload/dating" className="hover:squeezyBtn flex flex-col justify-center items-center bg-gradient-to-b from-[#e73e71] to-[#af1040] hover:from-[#bb2c57] hover:to-[#861436] text-[#F1F1F1] rounded-lg w-2/3 h-[58px] hover:shadow-[0_0_0_2px_#ffffff] transition duration-500">
-              Get your photos for $29
-            </Link>
-            </div>
+    <div className="flex justify-between">
+      <div className="flex flex-col justify-center gap-6 w-1/2">
+        <div className="text-6xl font-semibold 2xl:text-[80px] leading-tight text-[#F1F1F1] w-full">
+          <div>
+            <span className="font-semibold bg-gradient-to-r from-[#e73e71] to-[#af1040]  inline-block text-transparent bg-clip-text">
+              Double
+            </span>{" "}
+            your
+          </div>
+          <div>matches with our</div>
+          <div>AI dating photos</div>
+        </div>
+        <div className="text-[14px] 2xl:text-[16px] text-[#F1F1F1] text-left">
+          <div>
+            Use our AI headshot generator built by AI researchers to improve
+            your dating
+          </div>
+          <div>
+            profile in just minutes. Look your best to attract the best.
+          </div>
+        </div>
+        <div>
+          <Link
+            to="/upload/dating"
+            className="hover:squeezyBtn flex flex-col justify-center items-center bg-gradient-to-b from-[#e73e71] to-[#af1040] hover:from-[#bb2c57] hover:to-[#861436] text-[#F1F1F1] rounded-lg w-2/3 h-[58px] hover:shadow-[0_0_0_2px_#ffffff] transition duration-500"
+          >
+            Get your photos for $29
+          </Link>
+        </div>
       </div>
-      <div className='w-1/2 flex flex-col justify-center items-end'>
-        <img src="https://lh3.googleusercontent.com/d/11mgX3mlp1kIqIc9ehghQ8DRzD1GVBwvv=s800?authuser=0" className='h-full' alt="" />
+      <div className="w-1/2 flex flex-col justify-center items-end">
+        <img
+          src="https://lh3.googleusercontent.com/d/11mgX3mlp1kIqIc9ehghQ8DRzD1GVBwvv=s800?authuser=0"
+          className="h-full"
+          alt=""
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DatingHero
+export default DatingHero;
