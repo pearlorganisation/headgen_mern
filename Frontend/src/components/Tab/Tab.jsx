6c15ace5c61cb8ff661ad2351b4344dea5fa0a6e
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-06-11 16:20:07 +0530

    worked on headshot section

--- Frontend/src/components/Tab/Tab.jsx
+++ Frontend/src/components/Tab/Tab.jsx
@@ -30,56 +30,45 @@ const Tab = () => {
   const headshots = [
     {
       name: "Corporate Headshots",
+      headshotInfo: {
+        imgPath: "https://drive.google.com/thumbnail?id=15m130osnWyGu65RwKQ17HWpfIuKAMra6&sz=s800",
+      },
+      imgPreview: 'https://drive.google.com/thumbnail?id=1cNF1y4bnCERR4mFDYI60POS3w-a4rXy9&sz=s400'
     },
     {
       name: "Doctor Headshots",
       headshotInfo: {
-        tag: "Doctor Headshots",
-        title: "Professional Medical, Doctor, & ERAS Headshots with AI",
-        content:
-          "Elevate your healthcare profile with professional AI-generated medical headshots—perfect for doctors, medical school applications, or ERAS residency. Built by AI researchers from Meta and Microsoft.",
-        imgPath: "/headshots/healthcare.webp",
+        imgPath: "https://drive.google.com/thumbnail?id=1gXGt0a95L5qIZGzTaHaIUcrd2ppL32lv&sz=s800",
       },
+      imgPreview: 'https://drive.google.com/thumbnail?id=1UJpOzDSR_A5w6hA4D6zjfrnKMExF9vbC&sz=s400'
     },
     {
       name: "Lawyer Headshots",
       headshotInfo: {
-        tag: "Lawyer Headshots",
-        title: "Professional Lawyer Headshots with AI",
-        content:
-          "Get professional attorney headshots in minutes with the most realistic AI headshot generator ever. Built by AI researchers from Meta and Microsoft.",
-        imgPath: "/headshots/lawyer.webp",
+        imgPath: "https://drive.google.com/thumbnail?id=16p1K3X3OQMCpqW4nl8ylAOlTVsCjjPPS&sz=s800",
       },
+      imgPreview: 'https://drive.google.com/thumbnail?id=1bncFGpxo0FTHu8tUHgLiB3D_jmIMJE0Y&sz=s400'
     },
     {
       name: "Sales Headshots",
       headshotInfo: {
-        tag: "Sales Headshots",
-        title: "Close more leads with our charming AI headshots",
-        content:
-          "Update your professional headshot in minutes with the most realistic AI headshot generator ever. Built by AI researchers from Meta and Microsoft.",
-        imgPath: "/headshots/sales.webp",
+        imgPath: "https://drive.google.com/thumbnail?id=13SRDkSNJKlIj9qWD5sGKPQSGAyXSNJJW&sz=s800",
       },
+      imgPreview: 'https://drive.google.com/thumbnail?id=1-2l3yZmBK085Zbirg_D8OiHjVD3-r7HM&sz=s400'
     },
     {
       name: "Students Headshots",
       headshotInfo: {
-        tag: "Students Headshots",
-        title: "Land more jobs with our professional AI headshots",
-        content:
-          "Get affordable professional headshots in minutes with the most realistic AI headshot generator ever. Built by AI researchers from Meta and Microsoft.",
-        imgPath: "/headshots/student.webp",
+        imgPath: "https://drive.google.com/thumbnail?id=1A4yCODABo4qCZn7cRPlEdTUuSy5w-gLl&sz=s800",
       },
+      imgPreview: 'https://drive.google.com/thumbnail?id=1PRgcUxiH3r2NMbqDePx2eF9V7NVHX7nY&sz=s400'
     },
     {
       name: "Teacher Headshots",
       headshotInfo: {
-        tag: "Teacher Headshots",
-        title: "The best AI headshots for teachers",
-        content:
-          "Get professional headshots in minutes with the most realistic AI headshot generator ever. Perfect for school websites, learning platform, and more. Set a good first impression for students and their parents.",
-        imgPath: "/headshots/teacher.webp",
+        imgPath: "https://drive.google.com/thumbnail?id=1cGdkBJVVXcCwxo3CnErPumzgdwccwvLo&sz=s800",
       },
+      imgPreview: 'https://drive.google.com/thumbnail?id=1lvE4qi0vK9gqHeEZJ6JFn96D92NEPfOM&sz=s400'
     },
   ];
 
